2b816d6221b457a072243cbbfb6f77a59679e544
José Ivan Amorim <joseivan.ajr@gmail.com>
2023-08-21 20:18:00 +0000

    Adicionar 1 cena e 1 imagem

---
+++ cena_0.js
@@ -0,0 +1,15 @@
+export default class cena_0 extends Phaser.Scene {
+	constructor () {
+		super('cena_0')
+	}
+
+	preload(){
+		this.load.image('ifsc-sj-2014', './ifsc-sj-2014.png')
+	}
+
+	create(){
+		this.add.image(640, 360, 'ifsc-sj-2014')
+	}
+
+	update(){}
+}
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import config from "./config.js"
+import cena_0 from "./cena_0.js"
 
 class Game extends Phaser.Game {
-    constructor(){
-        super(config)
-    }
+	constructor(){
+		super(config)
+		this.scene.add('cena_0', cena_0)
+		this.scene.start('cena_0')
+	}
 }
 
 window.onload = () => {
-    window.game = new Game()
+	window.game = new Game()
 }
