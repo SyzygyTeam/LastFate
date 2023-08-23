f9d481f7b159d5d5a6b663222ea50352f7877d59
José Ivan Amorim <joseivan.ajr@gmail.com>
2023-08-23 20:19:13 +0000

    Instalar o ESLint

---
+++ cliente/js/cena0.js
@@ -0,0 +1,15 @@
+export default class cena0 extends Phaser.Scene {
+  constructor () {
+    super('cena_0')
+  }
+
+  preload () {
+    this.load.image('ifsc-sj-2014', '../assets/ifsc-sj-2014.png')
+  }
+
+  create () {
+    this.add.image(640, 360, 'ifsc-sj-2014')
+  }
+
+  update () { }
+}
--- cliente/js/index.js
+++ cliente/js/index.js
@@ -1,14 +1,14 @@
-import config from "./config.js"
-import cena_0 from "./cena_0.js"
+import config from './config.js'
+import cena0 from './cena0.js'
 
 class Game extends Phaser.Game {
-	constructor () {
-		super(config)
-		this.scene.add('cena_0', cena_0)
-		this.scene.start('cena_0')
-	}
+  constructor () {
+    super(config)
+    this.scene.add('cena_0', cena0)
+    this.scene.start('cena_0')
+  }
 }
 
 window.onload = () => {
-	window.game = new Game()
+  window.game = new Game()
 }
