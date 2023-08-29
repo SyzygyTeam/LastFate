148f1917450c38c2d1d785339d82ffa7a8e5bba9
José Ivan Amorim <joseivan.ajr@gmail.com>
2023-08-29 02:10:49 +0000

    Modelo básico de carta

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -5,10 +5,19 @@ export default class cena0 extends Phaser.Scene {
 
   preload () {
     this.load.image('ifsc-sj-2014', '../assets/ifsc-sj-2014.png')
+    this.load.image('card', '../assets/cards_bg/green_bg.png')
   }
 
   create () {
     this.add.image(640, 360, 'ifsc-sj-2014')
+    this.card = this.add.sprite(200, 200, 'card', 0)
+      .setInteractive()
+      .on('pointerdown', () => { })
+    this.input.setDraggable(this.card)
+    this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
+      gameObject.x = dragX
+      gameObject.y = dragY
+    })
   }
 
   update () { }
