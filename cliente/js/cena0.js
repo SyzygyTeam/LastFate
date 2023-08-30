1ed062ad515c75b0956ffa4abae59fb7a25724cc
José Ivan Amorim <joseivan.ajr@gmail.com>
2023-08-30 20:09:24 +0000

    Adicionar preload de fonte

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -4,11 +4,22 @@ export default class cena0 extends Phaser.Scene {
   }
 
   preload () {
+    this.load.script(
+      'webfont',
+      'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
+    )
     this.load.image('ifsc-sj-2014', '../assets/ifsc-sj-2014.png')
     this.load.image('card', '../assets/cards_bg/green_bg.png')
   }
 
   create () {
+    WebFont.load({
+      custom: {
+        families: ['PressStart2P'],
+        urls: ['../main.css']
+      }
+    })
+
     this.add.sprite(640, 360, 'ifsc-sj-2014')
       .setInteractive()
       .on('pointerdown', () => {
