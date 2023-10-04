26c011080997b9b0974198075bd24b08b4e7f6ad
José Ivan Amorim <joseivan.ajr@gmail.com>
2023-10-04 19:11:25 +0000

    Estruturado carta como Classe

--- cliente/js/Card.js
+++ cliente/js/Card.js
@@ -1,8 +1,13 @@
+/* Classe que gera, define e gerencia os Cards */
+/* Utiliza como base a classe 'Container' do Phaser */
+
+/* global Phaser */
 export default class Card extends Phaser.GameObjects.Container {
-  constructor (scene, centerX, centerY, cost, attack, health) {
+  constructor (scene, centerX, centerY, cardInfo) {
     /* Construção do Container */
     super(scene, centerX, centerY)
 
+    /* Definição da cena do Game */
     this.scene = scene
 
     /* Definição de valores p/ criação de txt */
@@ -11,59 +16,117 @@ export default class Card extends Phaser.GameObjects.Container {
     this.attackTxt = null
     this.healthTxt = null
 
-    this.costTxtShadow = null
-    this.attackTxtShadow = null
-    this.healthTxtShadow = null
-
-    this.arrValues = [cost, attack, health]
+    this.arrValues = [cardInfo.cost, cardInfo.attack, cardInfo.health]
     this.arrTxtElements = [this.costTxt, this.attackTxt, this.healthTxt]
-    this.arrTxtShadowElements = [this.costTxtShadow, this.attackTxtShadow, this.healthTxtShadow]
 
     /* Valores de posição dos elementos */
-    this.costPos = [-180, -280]
-    this.attackPos = [-180, 245]
-    this.healthPos = [140, 245]
+    this.costPos = [-100, -160]
+    this.attackPos = [-100, 140]
+    this.healthPos = [100, 140]
     this.arrPosElements = [this.costPos, this.attackPos, this.healthPos]
 
-    /* Valor de offset das sombras */
-    this.shadowOffset = 3
-
-    /* Base do Container - Card Background */
-    this.bgImg = this.scene.add.sprite(0, 0, 'card_bg')
+    /* Sprites */
+    this.bgImg = this.scene.add.sprite(0, 0, 'cardBg')
+    this.spriteImg = this.scene.add.sprite(0, -50, cardInfo.path)
+      .setScale(2)
     this.setSize(this.bgImg.width, this.bgImg.height)
 
-    /* Adição dos elementos no Container */
-    this.add(this.bgImg)
-    this.generate_txt()
+    /* Efeito de Hover de Board */
+    this.bgEffect = this.scene.add.rectangle(
+      0,
+      0,
+      260,
+      360,
+      0xFAFAFA
+    ).setVisible(false)
 
-    /* Escala Base do Card */
-    this.setScale(0.5)
+    this.effectCounter = this.scene.tweens.addCounter({
+      from: 0.2,
+      to: 0.8,
+      duration: 400,
+      yoyo: true,
+      loop: -1,
+      onUpdate: (tween) => {
+        const alpha = tween.getValue().toFixed(2)
+        this.bgEffect.setAlpha(alpha)
+      }
+    })
+
+    /* Texto de Título e Descrição */
+    this.nameTxt = this.scene.add.text(
+      20,
+      -157,
+      cardInfo.name,
+      {
+        resolution: 8,
+        fontFamily: 'PressStart2P',
+        color: '#050505',
+        fontSize: '15px'
+      }
+    )
+      .setOrigin(0.5, 0)
+
+    this.descriptionTxt = this.scene.add.text(
+      0,
+      55,
+      cardInfo.description,
+      {
+        fontFamily: 'PressStart2P',
+        fontSize: '15px',
+        resolution: 8,
+        color: '#050505',
+        align: 'center',
+        wordWrap: {
+          width: 250,
+          useAdvancedWrap: true
+        }
+      }
+    )
+      .setOrigin(0.5, 0)
+
+    /* Adição dos elementos no Container */
+    this.add(this.bgEffect)
+    this.add(this.bgImg)
+    this.add(this.spriteImg)
+    this.add(this.nameTxt)
+    this.add(this.descriptionTxt)
+    this.generateTxtValues() // Loop p/ implementação do texto de valores
+
+    /* Escala Base */
+    this.setScale(0.7)
 
     /* Interatividade */
     this.setInteractive()
     this.on('pointerdown', () => {
-      this.setScale(0.7)
-      this.y -= 170
+      this.setScale(1)
+      this.y -= 70
+      this.effectCounter.paused = false
     })
+
     this.on('pointerup', () => {
-      this.setScale(0.5)
+      this.effectCounter.paused = true
+      if (this.y < 300) {
+        this.play()
+      }
+      this.setScale(0.7)
       this.x = centerX
       this.y = centerY
     })
 
-    /* Adicionar na cena */
-    scene.add.existing(this)
-    scene.input.setDraggable(this)
+    /* Adicionar Container na cena */
+    this.scene.add.existing(this)
+    this.scene.input.setDraggable(this)
 
-    /* Elementos do card */
-    this.cost = cost
-    // this._name = name
-    this.attack = attack
-    this.health = health
-    // this.description = description
+    /* Elementos e Valores */
+    this._name = cardInfo.name
+    this.cost = cardInfo.cost
+    this.attack = cardInfo.attack
+    this.health = cardInfo.health
+    this.description = cardInfo.description
   }
 
-  generate_txt () {
+  generateTxtValues () {
+    /* Loop p/ geração dos valores numéricos */
     for (let i = 0; i < this.arrValues.length; i++) {
       this.arrTxtElements[i] = this.scene.add.text(
         this.arrPosElements[i][0],
@@ -71,21 +134,24 @@ export default class Card extends Phaser.GameObjects.Container {
         this.arrValues[i],
         {
           fontFamily: 'PressStart2P',
-          fontSize: '35px',
-          fill: 'white'
+          fontSize: '20px',
+          resolution: 2,
+          color: '#FAFAFA',
+          stroke: '#050505',
+          strokeThickness: 2,
+          shadow: {
+            offsetX: 4,
+            offsetY: 4,
+            fill: true
+          }
         })
+        .setOrigin(0.5, 0)
 
-      this.arrTxtShadowElements[i] = this.scene.add.text(
-        this.arrPosElements[i][0] + this.shadowOffset,
-        this.arrPosElements[i][1] + this.shadowOffset,
-        this.arrValues[i],
-        {
-          fontFamily: 'PressStart2P',
-          fontSize: '34px',
-          fill: 'black'
-        })
-      this.add(this.arrTxtShadowElements[i])
       this.add(this.arrTxtElements[i])
     }
+  }
+
+  play () {
+    this.destroy()
   }
 }
